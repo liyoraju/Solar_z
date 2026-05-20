bcd27ce14bd63278f9f3bd26be307a3276cb8e50
liyoraju <liyoraju7@gmail.com>
2026-05-20 13:10:54 +0530

    removed 5s,5m,1month interval

---
+++ api/static/assets/index-DkyV9ZkJ.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-C90AfTEQ.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- api/static/assets/index-NGTWUMiV.js
+++
@@ -1,1 +0,0 @@
-import{r as i}from"./index-Cafv5nNc.js";const t=i("PushNotifications",{});export{t as PushNotifications};
